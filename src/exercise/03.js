ddf6a97665702014fda94657d99f4a87b72509cc
Xavi Guasch <xaviguasch@gmail.com>
2023-05-07 17:06:34 +0200

    solved extra credit 1 exercise from the previous one

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -3,11 +3,13 @@
 
 import * as React from 'react'
 
-function Name({ name, onNameChange }) {
+function Name() {
+  const [name, setName] = React.useState('')
+
   return (
     <div>
       <label htmlFor='name'>Name: </label>
-      <input id='name' value={name} onChange={onNameChange} />
+      <input id='name' value={name} onChange={(event) => setName(event.target.value)} />
     </div>
   )
 }
@@ -22,25 +24,23 @@ function FavoriteAnimal({ animal, onAnimalChange }) {
   )
 }
 
-// 🐨 uncomment this
-function Display({ name, animal }) {
-  return <div>{`Hey ${name}, your favorite animal is: ${animal}!`}</div>
+function Display({ animal }) {
+  return <div>{`Your favorite animal is: ${animal}!`}</div>
 }
 
 function App() {
   // 🐨 add a useState for the animal
   const [animal, setAnimal] = React.useState('')
-  const [name, setName] = React.useState('')
   return (
     <form>
-      <Name name={name} onNameChange={(event) => setName(event.target.value)} />
+      <Name />
       {/* 🐨 pass the animal and onAnimalChange prop here (similar to the Name component above) */}
       <FavoriteAnimal
         animal={animal}
         onAnimalChange={(event) => setAnimal(event.target.value)}
       />
       {/* 🐨 pass the animal prop here */}
-      <Display name={name} animal={animal} />
+      <Display animal={animal} />
     </form>
   )
 }
